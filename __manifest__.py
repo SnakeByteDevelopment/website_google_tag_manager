053ca49825d11d0b029ad4157475b8bdb5dc22b3
Gert Pellin <gert@pellin.be>
2026-09-18 13:02:30 +0000

    [IMP] version 18.0.1.0

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': "Google Tag Manager integration",
-    'version': '1.0',
+    'version': '18.0.1.0',
     'depends': ['base', 'website'],
     'author': "Gert Pellin",
     'category': 'website',
